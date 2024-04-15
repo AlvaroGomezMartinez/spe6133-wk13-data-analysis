a33b51eaa44ad498f812b7edb2b5db1f49b5be93
Alvaro Gomez <alvaro.gomez2011@gmail.com>
2024-04-14 23:27:52 -0500

    Updates.

--- Wk13-RScript.R
+++ Wk13-RScript.R
@@ -1,3 +1,20 @@
 # Analyzing Baseline Data
 Getcsv()
 attach(ssd)
+
+## Reference these windows for the lecture recording:
+SSDAnalysis <- "https://ssdanalysis.com/videos"
+browseURL(SSDAnalysis)
+
+Posit <- "https://posit.co/download/rstudio-desktop/"
+browseURL(Posit)
+
+TheRProject <- "https://www.r-project.org/"
+browseURL(TheRProject)
+
+StandardCelerationChart <- "https://www.celeration.org/introduction/"
+browseURL(StandardCelerationChart)
+
+OgdenLindsley <- "http://precisionteaching.pbworks.com/w/page/18241059/Ogden%20Lindsley%20(1922-2004)"
+browseURL(OgdenLindsley)
+
